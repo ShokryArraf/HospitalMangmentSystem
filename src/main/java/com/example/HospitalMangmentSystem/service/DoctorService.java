c5e49ccf483e110489c469d83e22e1b5b97099a8
Shokry Arraf <arraf.shokry.as@gmail.com>
2025-10-27 20:03:43 +0200

    step-7(added some good functions to DoctorService)

--- src/main/java/com/example/HospitalMangmentSystem/service/DoctorService.java
+++ src/main/java/com/example/HospitalMangmentSystem/service/DoctorService.java
@@ -3,11 +3,12 @@ package com.example.HospitalMangmentSystem.service;
 import com.example.HospitalMangmentSystem.dto.DoctorCreateDto;
 import com.example.HospitalMangmentSystem.dto.DoctorDto;
 import com.example.HospitalMangmentSystem.exception.DuplicateResourceException;
+import com.example.HospitalMangmentSystem.exception.InvalidOperationException;
 import com.example.HospitalMangmentSystem.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class DoctorService {
@@ -65,5 +66,72 @@ public class DoctorService {
         dto.setRating(doctorCreateDto.getRating());
         dto.setSpecialization(doctorCreateDto.getSpecialization());
         return dto;
+    }
+
+    // getDoctorsBySpecializationMap() → Map<String, List<DoctorDto>>
+    public Map<String,List<DoctorDto>> getDoctorsBySpecialization() {
+        if (doctors.isEmpty()) {
+            throw new ResourceNotFoundException("No doctors found");
+        }
+        return doctors.stream()
+                .collect(Collectors.groupingBy(DoctorDto::getSpecialization));
+    }
+    //getTopRatedDoctors(int limit) → List<DoctorDto>
+    public List<DoctorDto> getTopRatedDoctors(int limit) {
+        if (doctors.isEmpty()) {
+            throw new ResourceNotFoundException("No doctors found");
+        }
+
+        if (limit <= 0) {
+            throw new InvalidOperationException("Limit must be greater than 0");
+        }
+
+        return doctors.stream()
+                .sorted(Comparator.comparingDouble(DoctorDto::getRating).reversed()) // sort by rating descending
+                .limit(limit) // take top N
+                .collect(Collectors.toList());
+    }
+
+    //. getDoctorsByExperienceRange(int minYears, int maxYears) →
+    //List<DoctorDto>
+    public List<DoctorDto> getDoctorsByExperienceRange(int minYears, int maxYears) {
+        if (doctors.isEmpty()) {
+            throw new ResourceNotFoundException("No doctors found");
+        }
+        if (minYears > maxYears) {
+            throw new InvalidOperationException("Min years must be greater than max years");
+        }
+        List<DoctorDto> doctorByExperienceRange = new ArrayList<>();
+        for (DoctorDto d : doctors) {
+            if (d.getYearsOfExperience() >= minYears && d.getYearsOfExperience() <= maxYears) {
+                doctorByExperienceRange.add(d);
+            }
+        }
+        doctorByExperienceRange.sort(Comparator.comparingInt(DoctorDto::getYearsOfExperience).reversed());
+        return doctorByExperienceRange;
+    }
+    // getAverageRatingBySpecialization() → Map<String, Double>
+    public Map<String,Double> getAverageRatingBySpecialization() {
+        if (doctors.isEmpty()) {
+            throw new ResourceNotFoundException("No doctors found");
+        }
+        return doctors.stream().collect(Collectors.groupingBy(DoctorDto::getSpecialization,Collectors.averagingDouble(DoctorDto::getRating)));
+    }
+    //getDoctorsWithMostAppointments(int limit) → List<DoctorDto>
+    public List<DoctorDto> getDoctorsWithMostAppointments(int limit) {
+        if (doctors.isEmpty()) {
+            throw new ResourceNotFoundException("No doctors found");
+        }
+        if (limit <= 0) {
+            throw new InvalidOperationException("Limit must be greater than 0");
+        }
+        List<DoctorDto> sortedDoctors = new ArrayList<>(doctors);
+        sortedDoctors.sort((d1,d2) -> Integer.compare(d2.getAppointmentIds().size(), d1.getAppointmentIds().size()));
+        // Take top limit
+        List<DoctorDto> topDoctors = new ArrayList<>();
+        for (int i = 0; i < limit && i < sortedDoctors.size(); i++) {
+            topDoctors.add(sortedDoctors.get(i));
+        }
+        return topDoctors;
     }
 }
